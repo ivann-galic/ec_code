c0252690b98c5c5324a9ac59bd72e9fd8a243916
Ivann <ivann.galic@edu.itescia.fr>
2020-06-24 10:01:48 +0200

    details() : display all infos from a media

--- view/details.php
+++ view/details.php
@@ -14,19 +14,17 @@
 
 
 <body>
-<?php
 
-include 'Outils.php';
-
-    $data = $req->fetch()
-
+    <?php
+        $data = $req->fetch()
     ?>
+
 <div class="wrapper d-flex align-items-stretch">
     <nav id="sidebar">
         <h2 class="title">Bienvenue</h2>
         <div class="sidebar-menu">
             <ul>
-                <li class="active"><a href="/CodFlix/">Médias</a></li>
+                <li class="active"><a href="index.php?action=mediasList">Médias</a></li>
                 <li><a href="#">Nous contacter</a></li>
                 <li><a href="index.php?action=logout">Me déconnecter</a></li>
             </ul>
@@ -45,7 +43,18 @@ include 'Outils.php';
             </div>
         </div>
         <div class="content p-4">
-            <p class="textTest">test: <?php echo $data['title'] ?></p>
+            <div class="row">
+                <div class="media_infos col-lg-12 col-md-12 col-sm-12 text-center">
+                    <iframe style='width:640px; height:368px' allowfullscreen="" frameborder="0"
+                            src="<?= $data['trailer_url']; ?>" ></iframe>
+                    <h1 class="media_title"><?php echo $data['title'] ?></h1>
+                    <p class="media_type"> <?php echo $data['type'] ?></p>
+                    <p class="media_status"> <?php echo $data['status'] ?> le <?php echo $data['release_date'] ?></p>
+                    <p class="media_summary col-md-10 offset-1 text-justify"> <?php echo $data['summary'] ?></p>
+                    <!--<p class="media_type"> <?php /*echo $data['type'] */?></p>-->
+                </div>
+
+            </div>
         </div>
         <footer>Copyright Cod'Flix</footer>
     </div>
