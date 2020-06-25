df24fd11f1052628b6519e5a75225ca692757b51
Ivann <ivann.galic@edu.itescia.fr>
2020-06-25 14:49:59 +0200

    profil page : user can also change mail and delete account (with warnings) - page finished

--- controller/profilController.php
+++ controller/profilController.php
@@ -23,15 +23,57 @@ function changePassword($id) {
 
     $current_password = hash("sha256", htmlentities($_POST['current_password']));
     $new_password = hash("sha256", htmlentities($_POST['new_password']));
-    $db = init_db();
-    $verify_pwd = $db->query('SELECT * FROM user WHERE id = \'' . $id . '\';');
+    $db_password = init_db();
+    $verify_pwd = $db_password->query('SELECT * FROM user WHERE id = \'' . $id . '\';');
     $user = $verify_pwd->fetch();
     if ($user['password'] == $current_password) {
         echo'<div class="alert alert-success alert_profil_password" role="alert">Votre mot de passe a bien été enregistré.</div>';
-            $change_pwd = $db->query('UPDATE user SET password = \'' . $new_password . '\' WHERE id = \'' . $id . '\';');
+            $change_pwd = $db_password->query('UPDATE user SET password = \'' . $new_password . '\' WHERE id = \'' . $id . '\';');
     } else {
             echo'<div class="alert alert-danger alert_profil_password" role="alert">Votre mot de passe actuel n\'est pas correct.</div>';
     }
 
-    $db   = null;
+    $db_password   = null;
+}
+
+/***************************
+ * ----- CHANGE EMAIL -----
+ ***************************/
+
+function changeEmail($id) {
+
+    $new_mail = htmlentities($_POST['new_email']);
+    $db_email = init_db();
+
+    $verify_mail = $db_email->prepare('SELECT email FROM user WHERE email = ?');
+    $verify_mail->execute(array($new_mail));
+    $user = $verify_mail->fetchAll();
+    if ($user) {
+        echo'<div class="alert alert-danger" role="alert">Cette adresse email est déjà utilisée.</div>';
+    } else {
+        echo'<div class="alert alert-success" role="alert">Votre adresse email a bien été modifiée.</div>';
+        $change_email = $db_email->query('UPDATE user SET email = \'' . $new_mail . '\' WHERE id = \'' . $id . '\';');
+    }
+
+    $db_email   = null;
+}
+
+/***************************
+ * ----- DELETE ACCOUNT -----
+ ***************************/
+
+function deleteAccount($id) {
+
+    $current_password = hash("sha256", htmlentities($_POST['delete_account_password']));
+    $db_delete_account = init_db();
+    $verify_pwd = $db_delete_account->query('SELECT * FROM user WHERE id = \'' . $id . '\';');
+    $user = $verify_pwd->fetch();
+    if ($user['password'] == $current_password) {
+        echo'<div class="alert alert-success alert_profil_password" role="alert">Votre compte a été supprimé.</div>';
+        $delete = $db_delete_account->query('DELETE FROM user WHERE id = \'' . $id . '\';');
+    } else {
+        echo'<div class="alert alert-danger alert_profil_password" role="alert">Votre mot de passe n\'est pas correct.</div>';
+    }
+
+    $db_email   = null;
 }
--- view/profil.php
+++ view/profil.php
@@ -4,10 +4,11 @@
         $profil = $req->fetch();
     ?>
 
-    <div class="row">
+    <div class="row body_current">
         <div class="title_profile_section col-sm-8 col-md-8 col-lg-6 col-xl-6 offset-2">
             <h1>Votre profil</h1>
         </div>
+
         <div class="modify_password_section col-sm-8 col-md-8 col-lg-6 col-xl-6 offset-2">
             <h2>Modifier votre mot de passe</h2>
 <!--            <p>--><?//= isset( $error_msg ) ? $error_msg : null; ?><!--</p>-->
@@ -19,24 +20,58 @@
                             changePassword($profil['id']);
                         }
                         ?>
-                        <label for="input_name">Veuillez saisir votre mot de passe actuel :</label>
+                        <label for="current_password">Veuillez saisir votre mot de passe actuel :</label>
                         <input type="text" name="current_password" class="form-control" id="input_current_password" required>
                     </div>
                 </div>
 
                 <div class="row">
                     <div class="form-group col-sm-8 col-md-8 col-lg-8 col-xl-8">
-                        <label for="input_name">Veuillez saisir votre nouveau mot de passe :</label>
+                        <label for="new_password">Veuillez saisir votre nouveau mot de passe :</label>
                         <input type="text" name="new_password" class="form-control" id="input_new_password" required>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary" name="change_password">Valider</button>
             </form>
         </div>
+
         <div class="modify_mail_section col-sm-8 col-md-8 col-lg-6 col-xl-6 offset-2">
             <h2>Modifier votre adresse email</h2>
             <p><span class="your_mail">Votre adresse email actuelle :</span> <?php echo $profil['email'] ?></p>
+            <form action="#" method="POST">
+                <div class="row">
+                    <div class="form-group col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                        <?php
+                        if(isset($_POST['change_email'])){
+                            changeEmail($profil['id']);
+                        }
+                        ?>
+                        <label for="new_email">Veuillez saisir votre nouvelle adresse email :</label>
+                        <input type="text" name="new_email" class="form-control" id="input_new_email" required>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary" name="change_email">Valider</button>
+            </form>
         </div>
+
+        <div class="modify_mail_section col-sm-8 col-md-8 col-lg-6 col-xl-6 offset-2">
+            <h2>Supprimer votre compte</h2>
+            <form action="#" method="POST">
+                <div class="row">
+                    <div class="form-group col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                        <?php
+                        if(isset($_POST['delete_account'])){
+                            deleteAccount($profil['id']);
+                        }
+                        ?>
+                        <label for="delete_account_password">Veuillez confirmer avec votre mot de passe :</label>
+                        <input type="text" name="delete_account_password" class="form-control" id="delete_account_password" required>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary" name="delete_account">Valider</button>
+            </form>
+        </div>
+
     </div>
 
 
